f36d40ef444575f33b22a94c14dd7e0f28165c45
Joaquin Nuñez <juacoandres123@gmail.com>
2025-06-26 15:01:55 -0400

    cambio vite

--- vite.config.js
+++ vite.config.js
@@ -4,13 +4,13 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base: './', // 👈 Añadido para producción en Vercel
   server: {
-    host: '0.0.0.0', // Escucha en todas las interfaces de red, permitiendo acceso desde la red local
-    https: false,    // Confirma que no usas HTTPS para desarrollo local
-    port: 5173,      // Puerto por defecto, mantenlo si no quieres cambiarlo
+    host: '0.0.0.0',
+    https: false,
+    port: 5173,
     hmr: {
-      // Opcional: Configura el protocolo si necesitas forzar ws o wss
-      // protocol: 'ws', // Descomenta solo si tienes problemas con HMR
+      // protocol: 'ws',
     },
   },
 });
